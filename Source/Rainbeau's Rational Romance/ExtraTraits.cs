a18a790f1ab594ef769a7348857f278de1c65b12
Kate Davis <kadadavi@iu.edu>
2025-11-14 00:04:59 -0500

    removed asexual just need to make all straight and gay, bi

--- Source/Rainbeau's Rational Romance/ExtraTraits.cs
+++ Source/Rainbeau's Rational Romance/ExtraTraits.cs
@@ -12,64 +12,10 @@ public static class ExtraTraits
         {
             return;
         }
+        // Force every pawn to be bisexual to avoid assignment of asexual/straight/gay.
+        pawn.story.traits.GainTrait(new Trait(TraitDefOf.Bisexual));
 
-        if (orientation < RationalRomance.Settings.AsexualChance / 100)
-        {
-            if (LovePartnerRelationUtility.HasAnyLovePartnerOfTheOppositeGender(pawn) ||
-                LovePartnerRelationUtility.HasAnyExLovePartnerOfTheOppositeGender(pawn))
-            {
-                pawn.story.traits.GainTrait(new Trait(TraitDefOf.Bisexual));
-            }
-            else if (LovePartnerRelationUtility.HasAnyLovePartnerOfTheSameGender(pawn) ||
-                     LovePartnerRelationUtility.HasAnyExLovePartnerOfTheSameGender(pawn))
-            {
-                pawn.story.traits.GainTrait(new Trait(TraitDefOf.Bisexual));
-            }
-            else if (pawn.story.traits.HasTrait(RRRTraitDefOf.Philanderer))
-            {
-                pawn.story.traits.GainTrait(new Trait(TraitDefOf.Bisexual));
-            }
-            else
-            {
-                pawn.story.traits.GainTrait(new Trait(TraitDefOf.Asexual));
-            }
-        }
-        else if (orientation < (RationalRomance.Settings.AsexualChance + RationalRomance.Settings.BisexualChance) /
-                 100)
-        {
-            pawn.story.traits.GainTrait(new Trait(TraitDefOf.Bisexual));
-        }
-        else if (orientation < (RationalRomance.Settings.AsexualChance + RationalRomance.Settings.BisexualChance +
-                                RationalRomance.Settings.GayChance) / 100)
-        {
-            if (LovePartnerRelationUtility.HasAnyLovePartnerOfTheOppositeGender(pawn) ||
-                LovePartnerRelationUtility.HasAnyExLovePartnerOfTheOppositeGender(pawn))
-            {
-                pawn.story.traits.GainTrait(new Trait(TraitDefOf.Bisexual));
-            }
-            else
-            {
-                pawn.story.traits.GainTrait(new Trait(TraitDefOf.Gay));
-            }
-        }
-        else
-        {
-            if (LovePartnerRelationUtility.HasAnyLovePartnerOfTheSameGender(pawn) ||
-                LovePartnerRelationUtility.HasAnyExLovePartnerOfTheSameGender(pawn))
-            {
-                pawn.story.traits.GainTrait(new Trait(TraitDefOf.Bisexual));
-            }
-            else
-            {
-                pawn.story.traits.GainTrait(new Trait(RRRTraitDefOf.Straight));
-            }
-        }
-
-        if (pawn.story.traits.HasTrait(TraitDefOf.Asexual) || pawn.story.traits.HasTrait(RRRTraitDefOf.Polyamorous))
-        {
-            return;
-        }
-
+        // Optionally assign polyamorous according to settings (preserve original behavior).
         if (Rand.Value < RationalRomance.Settings.PolyChance / 100)
         {
             pawn.story.traits.GainTrait(new Trait(RRRTraitDefOf.Polyamorous));
